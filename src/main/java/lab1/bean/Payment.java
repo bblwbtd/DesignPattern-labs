639039cb9d3b0cb9b9aa90d2dd72fe44e455f359
LiuJing-Luyigi <64572320+LiuJing-Luyigi@users.noreply.github.com>
2020-05-06 17:45:52 +0800

    Add files via upload

--- src/main/java/lab1/bean/Payment.java
+++ src/main/java/lab1/bean/Payment.java
@@ -5,44 +5,33 @@ public class Payment {
     private double interest;
     private String date;
     private int remainMonth;
+    private double rest;
 
-    public Payment(double redemption, double interest, int remainMonth) {
+    public Payment(double redemption,double rest, double interest, int remainMonth) {
         this.redemption = redemption;
+        this.rest = rest;
         this.interest = interest;
         this.remainMonth = remainMonth;
     }
 
+    public double getRest() {
+        return rest;
+    }
 
     public double getRedemption() {
         return this.redemption;
-    }
-
-    public void setRedemption(double var1) {
-        this.redemption = var1;
     }
 
     public double getInterest() {
         return this.interest;
     }
 
-    public void setInterest(double var1) {
-        this.interest = var1;
-    }
-
     public int getRemainMonth() {
         return remainMonth;
     }
 
-    public void setRemainMonth(int remainMonth) {
-        this.remainMonth = remainMonth;
-    }
-
     public String getDate() {
         return this.date;
-    }
-
-    public void setDate(String var1) {
-        this.date = var1;
     }
 
 }
